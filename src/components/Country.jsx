6970ffacf0ba15bfc8955d892b4f0d379dedf2c8
Onur Yüksekkaya <sosyal@onuryuksekkaya.com>
2021-07-24 22:17:12 +0300

    console log deleted

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,61 +2,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Country = (props) => {
-  console.log(props);
-  return (
-    <div className="mt-5">
-      <button type="button" className="back-button">
-        <Link to="/">Go Back</Link>
-      </button>
-      <div className="row">
-        <div className="col-lg-6 flag-full">
-          <img src={props.country.flag} alt="flag" />
-        </div>
-        <div className="col-lg-6 country-details">
-          <div className="country-name">{props.country.name}</div>
-          <div className="row">
-            <div className="col-lg-6">
-              <p>
-                Demonym: <span>{props.country.demonym}</span>
-              </p>
-              <p>
-                Region: <span>{props.country.region}</span>
-              </p>
-              <p>
-                Subregion: <span>{props.country.subregion}</span>
-              </p>
-              <p>
-                Capital: <span>{props.country.capital}</span>
-              </p>
-              <p>
-                Population:
-                <span>{props.country.population}</span>
-              </p>
-              <p>
-                Area: <span>{props.country.area}</span>
-              </p>
-              <p>
-                Code: <span>{props.country.callingCodes}</span>
-              </p>
-            </div>
-            <div className="col-lg-6">
-              <p>
-                Native Name:
-                <span>{props.country.nativeName}</span>
-              </p>
-              <p>
-                Cioc: <span>{props.country.cioc}</span>
-              </p>
-              <p>
-                Time Zones: <span>{props.country.timezones}</span>
-              </p>
-            </div>
+const Country = (props) => (
+  <div className="mt-5">
+    <button type="button" className="back-button">
+      <Link to="/">Go Back</Link>
+    </button>
+    <div className="row">
+      <div className="col-lg-6 flag-full">
+        <img src={props.country.flag} alt="flag" />
+      </div>
+      <div className="col-lg-6 country-details">
+        <div className="country-name">{props.country.name}</div>
+        <div className="row">
+          <div className="col-lg-6">
+            <p>
+              Demonym: <span>{props.country.demonym}</span>
+            </p>
+            <p>
+              Region: <span>{props.country.region}</span>
+            </p>
+            <p>
+              Subregion: <span>{props.country.subregion}</span>
+            </p>
+            <p>
+              Capital: <span>{props.country.capital}</span>
+            </p>
+            <p>
+              Population:
+              <span>{props.country.population}</span>
+            </p>
+            <p>
+              Area: <span>{props.country.area}</span>
+            </p>
+            <p>
+              Code: <span>{props.country.callingCodes}</span>
+            </p>
+          </div>
+          <div className="col-lg-6">
+            <p>
+              Native Name:
+              <span>{props.country.nativeName}</span>
+            </p>
+            <p>
+              Cioc: <span>{props.country.cioc}</span>
+            </p>
+            <p>
+              Time Zones: <span>{props.country.timezones}</span>
+            </p>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Country;
